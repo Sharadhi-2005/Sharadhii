954c5372d6f16e868c71c2843e14830827c27fc1
Sharadhi <sharadhigowda2005gmail.com>
2026-04-20 15:25:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connect } from 'mongoose';
+import connectDB from './data_base/connection.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World" });
+})
+app.get("/heart-beat", (req, res) => {
+    res.status(200).json({ message: "Server is alive" });
+})
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server is healthy" });
+})   
+
+
+// app.listen(port-number,function)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+});

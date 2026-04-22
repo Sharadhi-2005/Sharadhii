aa330e83c458318d36d085612c7edc0947d6badb
Sharadhi <sharadhigowda2005gmail.com>
2026-04-22 15:57:59 +0530

    first commit

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { connect } from 'mongoose';
 import connectDB from './data_base/connection.js';
+import authRouter from './routers/authrouthers.js';
+import gameRouter from './routers/gameRouters.js';
+import { product } from './middleware/authMiddleware.js';
+// import authRouter from './routers/authuthers.js';
 
 dotenv.config();
+
 const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT ;
 
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Hello World" });
+  res.status(200).json({message: "Hello, World!"});
 })
+
 app.get("/heart-beat", (req, res) => {
-    res.status(200).json({ message: "Server is alive" });
+  res.status(200).json({message: "Server is alive!"});
 })
-app.get("/health", (req, res) => {
-    res.status(200).json({ message: "Server is healthy" });
-})   
 
+app.post("/echo", (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ echo: `Hello, ${name}` });
+});
 
-// app.listen(port-number,function)
+app.use("/api/v1/auth", authRouter)
+
+app.use("/api/v1/game", gameRouter)
+
+// app.listen(port-number, function)
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
